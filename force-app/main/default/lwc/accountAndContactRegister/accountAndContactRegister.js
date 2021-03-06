c4a9b13a6e4f697c0eb817000c16de6988ead963
miskitamur2 <hoto.inzm@gmail.com>
2021-03-06 10:52:18 +0900

    取引先・取引先責任者の登録機能

---
+++ force-app/main/default/lwc/accountAndContactRegister/accountAndContactRegister.js
@@ -0,0 +1,89 @@
+import { createRecord } from 'lightning/uiRecordApi';
+import { api, LightningElement } from 'lwc';
+export default class AccountAndContactRegister extends LightningElement {
+    accountId;
+    accountName;
+    accountNameKana;
+    accountCode;
+    contactId;
+    lastName;
+    firstName;
+
+    onChangeAccountName(event){
+        this.accountName = event.target.value;
+    }
+
+    onChangeAccountNameKana(event){
+        this.accountNameKana = event.target.value;
+    }
+
+    onChangeAccountCode(event){
+        this.accountCode = event.target.value;
+    }
+
+    onChangeLastName(event){
+        this.lastName = event.target.value;
+    }
+
+    onChangeFirstName(event){
+        this.firstName = event.target.value;
+    }
+
+    handleCreate(){
+        const accountRec = {
+            apiName: 'Account',
+            fields: {
+                Name : this.accountName,
+                AccountKana__c : this.accountNameKana,
+                AccountNumber : this.accountCode
+            }
+        };
+        const contactRec = {
+            apiName : 'Contact',
+            fields : {
+                LastName : this.lastName,
+                FirstName : this.firstName
+            }
+        };
+        createRecord(accountRec)
+            .then(result => {
+                this.accountId = result.id;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Account created',
+                        variant: 'success',
+                    }),
+                );
+            })
+            .catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error creating record',
+                        message: error.body.message,
+                        variant: 'error',
+                    }),
+                );
+            });
+        createRecord(contactRec)
+            .then(result => {
+                this.contactId = result.id;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Contact created',
+                        variant: 'success',
+                    }),
+                );
+            })
+            .catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error creating record',
+                        message: error.body.message,
+                        variant: 'error',
+                    }),
+                );
+            });
+    }
+}
